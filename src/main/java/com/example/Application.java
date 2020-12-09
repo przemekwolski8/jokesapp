e8ef18dcb092e84b80cec55ad3c821b1ddb89448
przemekwolski8 <przemekwolski8@gmail.com>
2020-12-09 23:24:41 +0100

    remove shit added simple rest

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,13 @@
-package com.example.guru;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GuruApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(GuruApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/com/example/guru/services/JokeService.java
+++
@@ -1,6 +0,0 @@
-package com.example.guru.services;
-
-public interface JokeService {
-
-    String getJoke();
-}
